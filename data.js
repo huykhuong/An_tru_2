117ccfb777da22eecfc4e2fcda342bc4e372797b
Huy Khuong <scottkhuong@gmail.com>
2022-04-26 11:46:20 +0700

    26/4 pc

--- data.js
+++ data.js
@@ -20,22 +20,17 @@ export const mon_com = [
     type: "Cơm trắng",
   },
   {
-    name: "Cơm đậu hũ viên sốt nấm",
-    price: 73000,
-    type: "Cơm trắng",
-  },
-  {
     name: "Cơm nấm cây sốt Bulgogi",
     price: 73000,
     type: "Cơm trắng",
   },
   {
-    name: "Cơm nấm kho tiêu",
+    name: "Cơm Nấm kho sốt Tiêu",
     price: 60000,
     type: "Cơm trắng",
   },
   {
-    name: "Cơm nấm đậu hủ sốt cam",
+    name: "Cơm Nấm đậu hủ sốt Cam",
     price: 73000,
     type: "Cơm trắng",
   },
@@ -63,7 +58,7 @@ export const mon_soi = [
     type: "Món sợi (khô)",
   },
   {
-    name: "Mì trộn Hàn Quốc",
+    name: "Mì trộn Japchae",
     price: 73000,
     type: "Món sợi (khô)",
   },
@@ -79,19 +74,6 @@ export const mon_soi = [
   },
 ];
 
-export const mon_canh = [
-  {
-    name: "Canh rong biển tàu hủ",
-    price: 73000,
-    type: "Món canh",
-  },
-  {
-    name: "Canh tomyum nấm",
-    price: 47000,
-    type: "Món canh",
-  },
-]
-
 export const mon_rau = [
   {
     name: "Xà lách tương giấm/ mè đen",
@@ -104,7 +86,7 @@ export const mon_rau = [
     type: "Món rau",
   },
   {
-    name: "Rau bó xôi xào nấm mỡ",
+    name: "Cải thìa xào nấm mỡ",
     price: 73000,
     type: "Món rau",
   },
@@ -113,25 +95,126 @@ export const mon_rau = [
     price: 73000,
     type: "Món rau",
   },
-]
+];
 
 export const mon_an_kem = [
   {
     name: "Khoai tây viên chiên xù",
     price: 73000,
-    type: 'Món ăn kèm'
+    type: "Món ăn kèm",
   },
   {
     name: "Chả giò trái cây",
     price: 69000,
-    type: 'Món ăn kèm'
+    type: "Món ăn kèm",
   },
   {
     name: "Trứng ngâm tương",
     price: 73000,
-    type: 'Món ăn kèm'
+    type: "Món ăn kèm",
   },
-]
+  {
+    name: "Canh rong biển tàu hủ",
+    price: 73000,
+    type: "Món canh",
+  },
+  {
+    name: "Canh Tomyum nấm",
+    price: 47000,
+    type: "Món canh",
+  },
+];
+
+export const trang_mieng = [
+  {
+    name: "Kem Gelato (4 vị)",
+    price: 47000,
+    type: "Tráng miệng",
+  },
+  {
+    name: "Canh Tomyum nấm",
+    price: 47000,
+    type: "Tráng miệng",
+  },
+];
+
+export const do_uong = [
+  {
+    name: "Nước Detox",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Trà Tắc mật ong",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Trà Cam quế mật ong",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Trà lài/sen (đá/nóng)",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Nước ép Táo",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Nước ép Cam",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Nước ép Thơm",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Nước ép Cà rốt",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Soda Chanh",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Soda Chanh dây",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Soda Dâu",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Nước suối (chai thủy tinh)",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Dừa tắc Cocoxim",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Coca-cola",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+  {
+    name: "Pepsi",
+    price: 47000,
+    type: "Đồ Uống",
+  },
+];
 
 // export default {
 //   mon_com, mon_rau, mon_soi, mon_an_kem, mon_canh
