b529a10fce8da7ae3ff34f42a52f0b919862a11a
Huy Khuong <scottkhuong@gmail.com>
2022-05-01 20:52:25 +0700

    1/5 ver-3 pc

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -91,6 +91,7 @@ const Menu = () => {
         {/* <meta property="og:url" content="https://iron-fever.vn/"/> */}
         {/* <meta property="og:image:secure_url" content="https://bizweb.dktcdn.net/100/360/775/themes/729132/assets/logo.png?1622064036816"></meta> */}
         <meta property="og:site_name" content="Nhà hàng chay An Trú" />
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="#3A563E"
