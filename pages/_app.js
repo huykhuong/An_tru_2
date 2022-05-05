a461955ff68505986ba0a353dce9bd83464c4e32
Huy Khuong <scottkhuong@gmail.com>
2022-05-05 20:07:10 +0700

    5/5 ver-5 pc

--- pages/_app.js
+++ pages/_app.js
@@ -44,18 +44,25 @@ function MyApp({ Component, pageProps }) {
             content="width=device-width,minimum-scale=1,maximum-scale=1,initial-scale=1,user-scalable=no"
           />
           <meta name="theme-color" content="#3A563E"></meta>
+
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
           <link
             rel="icon"
             type="image/png"
-            sizes="256 x 256"
-            href="favicon.ico"
+            sizes="32x32"
+            href="/favicon-32x32.png"
           />
-          <link rel="apple-touch-icon" href="apple-touch-icon.png" />
           <link
-            rel="mask-icon"
-            href="apple-touch-icon.png"
-            color="#000000"
-          ></link>
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
         </Head>
 
         {loading ? (
