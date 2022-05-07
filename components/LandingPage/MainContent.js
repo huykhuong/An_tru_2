8582975999983177c4a625182220071c00ee865d
Huy Khuong <scottkhuong@gmail.com>
2022-05-07 16:16:28 +0700

    7/5 ver-6 pc

--- components/LandingPage/MainContent.js
+++ components/LandingPage/MainContent.js
@@ -87,22 +87,24 @@ const MainContent = () => {
               ></path>
             </svg>
           </Link>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="30"
-            height="30"
-            viewBox="0 0 24 24"
-            style={{ fill: "#000000" }}
-            className="inline-block mt-1 lg:cursor-pointer"
-          >
-            <path
-              id="phone-logo"
-              key={"phone-logo"}
-              d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
-            />
-          </svg>
+          <Link href="tel:0937930688">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              style={{ fill: "#000000" }}
+              className="inline-block mt-1 lg:cursor-pointer"
+            >
+              <path
+                id="phone-logo"
+                key={"phone-logo"}
+                d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
+              />
+            </svg>
+          </Link>
         </div>
       </section>
 
